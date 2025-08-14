af950c64448a3b614e890721f2055f24bb8508f9
Work <work@mac.lan>
2025-08-14 20:07:08 +0200

    Initial commit - dbt project setup

---
+++ target/compiled/gcp_dbt_project/models/example/customers.sql
@@ -0,0 +1,13 @@
+-- models/example/customers.sql
+WITH raw_customers AS (
+    SELECT *
+    FROM `sinuous-network-468812-j8.raw_olist.customers`
+)
+
+SELECT
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix,
+    customer_city,
+    customer_state
+FROM raw_customers
